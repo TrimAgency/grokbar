cfc0f4bdc98b3ff5e54789e71a56491e407f77ae
Ryan Stone <ryan@ryanstone.io>
2018-04-19 17:14:17 -0400

    Initial Commit

---
+++ GrokBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GrokBar
+//
+//  Created by Sharkmaul on 4/19/18.
+//  Copyright © 2018 ryanStone. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
